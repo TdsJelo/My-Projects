7c99e29fd6a7f81c92bfa404d2668f9551a955e9
TdsJelo <121742151+TdsJelo@users.noreply.github.com>
2023-03-19 22:14:26 +0800

    Add files via upload

---
+++ Odd and Even/Odd and Even/Program.cs
@@ -0,0 +1,40 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: JELO
+ * Date: 26/09/2022
+ * Time: 10:18 am
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+
+namespace Odd_and_Even
+{
+	class Program
+	{
+		public static void Main(string[] args)
+		{
+				do {
+				Console.Clear();
+				int number;
+			Console.WriteLine("Input your number:");
+			number= int.Parse(Console.ReadLine());
+			if (number % 2 == 0)
+			{
+				Console.WriteLine("Number is even");
+			}
+			else
+			{
+				Console.WriteLine("Number is odd");
+			}
+			Console.Write("Would you like to test again? (Y= yes & N= no): ");
+				}while(Console.ReadLine().ToUpper() == "Y");
+				
+				Console.WriteLine("Bye!");
+				{
+				Console.ReadKey();
+				}
+			}
+		}
+		
+	}
